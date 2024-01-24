6f4fd6c708646e87a9c8a670988e5876edde1ed8
Lazaro Tomas Del Prado <lazarodelprado97@gmail.com>
2024-01-24 13:13:25 -0300

    front normalizado con tailwind instalado

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <p className='text-center font-medium text-4xl py-4'>Administrador de usuarios</p>
+    </div>
+  )
+}
+
+export default App
